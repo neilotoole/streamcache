87dc62cee1bb050cbd28fd8c0d1ed7eff0769ada
Neil O'Toole <neilotoole@apache.org>
2021-02-25 22:33:33 -0700

    yet more early work

--- samplereader.go
+++ samplereader.go
@@ -1,6 +1,30 @@
 // Package samplereader implements a reader mechanism that allows
 // multiple callers to sample some or the entire contents of a
 // source reader, while only reading from the source reader once.
+//
+// This is admittedly a rather arcane need.
+// Let's say we're reading from stdin. For example:
+//
+//  # cat myfile.ext | myprogram
+//
+// In this scenario, myprogram wants to detect the type of data
+// in the file/pipe, and then print it out. The input file could be,
+// let's say, a CSV file, or a TSV file.
+//
+// The obvious approach is to inspect the first few lines of the
+// input, and check if the input is either valid CSV, or valid TSV.
+// After that process, let's say we want to dump out the entire contents
+// of the input.
+//
+// Package samplereader provides a facility to create a Source from an
+// underlying io.Reader (os.Stdin in this scenario), and spawn multiple
+// readers, each of which can operate independently, in their own
+// goroutines if desired. The underlying source (again, os.Stdin in this
+// scenario) will only be read from once, but its data is available to
+// multiple readers, because that data is cached in memory.
+// That is, until there's only one final reader left, (after invoking
+// Source.Seal) at which point the cache is discarded, and
+// the final reader reads straight from the underlying source.
 package samplereader
 
 import (
@@ -10,13 +34,12 @@ import (
 	"sync"
 )
 
-// Source provides a somewhat specialized reader/sampling
-// mechanism. It takes a source io.Reader, and allows many callers
-// to invoke NewReader, providing each with a reader that returns
-// the same data as the source reader. The data read from the source
-// is cached. Once Seal is invoked, no new readers can be created.
-// When there's only one reader remaining, the cache is dispensed
-// with, and that reader reads remaining data directly from source.
+// ErrSealed is returned by Source.NewReadCloser if the source is
+// already sealed.
+var ErrSealed = errors.New("already sealed")
+
+// Source encapsulates an underlying io.Reader that many callers
+// can read from.
 type Source struct {
 	src    io.Reader
 	mu     sync.Mutex
@@ -32,12 +55,9 @@ func NewSource(r io.Reader) *Source {
 	return &Source{src: r, buf: &bytes.Buffer{}}
 }
 
-// ErrSealed is returned by Source.NewReader if it is
-// already sealed.
-var ErrSealed = errors.New("already sealed")
-
-// NewReader returns a new reader for Source.
-func (s *Source) NewReader() (io.ReadCloser, error) {
+// NewReadCloser returns a new ReadCloser for Source. It is the caller's
+// responsibility to close the returned ReadCloser.
+func (s *Source) NewReadCloser() (*ReadCloser, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
@@ -46,7 +66,7 @@ func (s *Source) NewReader() (io.ReadCloser, error) {
 	}
 
 	s.count++
-	return &reader{s: s}, nil
+	return &ReadCloser{s: s}, nil
 }
 
 func (s *Source) readAt(p []byte, off int64) (n int, err error) {
@@ -104,30 +124,33 @@ func (s *Source) close() error {
 	return nil
 }
 
-// Seal results indicates that there will be no more calls
-// to NewReader.
+// Seal is called to indicate that there will be no more calls
+// to NewReadCloser.
 func (s *Source) Seal() {
 	s.mu.Lock()
 	s.sealed = true
 	s.mu.Unlock()
 }
 
-type reader struct {
+// ReadCloser is returned by Source.NewReadCloser.
+type ReadCloser struct {
 	s   *Source
 	off int // needs to be atomic
 }
 
-func (r *reader) Read(p []byte) (n int, err error) {
+// Read implements io.Reader.
+func (r *ReadCloser) Read(p []byte) (n int, err error) {
 	n, err = r.s.readAt(p, int64(r.off))
 	r.off += n
 
 	return n, err
 }
 
-// Close closes this reader. If the Source is sealed and this
-// is the last remaining reader, the underlying io.Reader is closed
-// by this method, if that reader implements io.Closer.
-func (r *reader) Close() error {
+// Close closes this reader. If the parent Source is not sealed, this
+// method is effectively a no-op. If the parent Source is sealed and
+// is the last remaining reader, the parent Source's underlying
+// io.Reader is closed, if it implements io.Closer.
+func (r *ReadCloser) Close() error {
 	return r.s.close()
 }
 
--- samplereader_test.go
+++ samplereader_test.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"os"
-	"path/filepath"
 	"strconv"
 	"strings"
 	"sync"
@@ -21,14 +20,18 @@ import (
 	"github.com/neilotoole/samplereader"
 )
 
+const sampleRows = 100000
+
+var _ io.ReadCloser = (*samplereader.ReadCloser)(nil)
+
 func TestSimple(t *testing.T) {
-	f := generateSampleFile(t, 100000)
+	f := generateSampleFile(t, sampleRows)
 	rc := &readCloser{Reader: f}
 	src := samplereader.NewSource(rc)
 	wantB, err := ioutil.ReadAll(f)
 	require.NoError(t, err)
 
-	r, err := src.NewReader()
+	r, err := src.NewReadCloser()
 	require.NoError(t, err)
 	src.Seal()
 
@@ -43,19 +46,19 @@ func TestSimple(t *testing.T) {
 	require.Equal(t, wantB, gotB)
 }
 
-func TestSampleReaderConcurrent(t *testing.T) {
-	f := generateSampleFile(t, 100000)
+func TestConcurrent(t *testing.T) {
+	f := generateSampleFile(t, sampleRows)
 	wantB, err := ioutil.ReadAll(f)
 	require.NoError(t, err)
 
 	rc := &readCloser{Reader: bytes.NewReader(wantB)}
-	sr := samplereader.NewSource(rc)
+	src := samplereader.NewSource(rc)
 	require.NoError(t, err)
 
 	g := &errgroup.Group{}
 	for i := 0; i < 10000; i++ {
 		i := i
-		r, err := sr.NewReader()
+		r, err := src.NewReadCloser()
 		require.NoError(t, err)
 
 		g.Go(func() error {
@@ -80,34 +83,34 @@ func TestSampleReaderConcurrent(t *testing.T) {
 			return nil
 		})
 	}
-	sr.Seal()
+	src.Seal()
 	err = g.Wait()
 	assert.NoError(t, err)
 	assert.Equal(t, 1, rc.closed)
 }
 
 func TestSeal(t *testing.T) {
-	sr := samplereader.NewSource(strings.NewReader(""))
-	r, err := sr.NewReader()
+	src := samplereader.NewSource(strings.NewReader(""))
+	r, err := src.NewReadCloser()
 	require.NoError(t, err)
 	require.NotNil(t, r)
 
-	sr.Seal()
-	r, err = sr.NewReader()
+	src.Seal()
+	r, err = src.NewReadCloser()
 	require.Error(t, err)
 	require.Equal(t, samplereader.ErrSealed, err)
 	require.Nil(t, r)
 }
 
 func TestClose(t *testing.T) {
-	f := generateSampleFile(t, 1000)
+	f := generateSampleFile(t, sampleRows)
 	wantB, err := ioutil.ReadAll(f)
 	require.NoError(t, err)
 
 	rc := &readCloser{Reader: f}
 	src := samplereader.NewSource(rc)
 
-	r1, err := src.NewReader()
+	r1, err := src.NewReadCloser()
 	require.NoError(t, err)
 
 	gotB1, err := ioutil.ReadAll(r1)
@@ -116,7 +119,7 @@ func TestClose(t *testing.T) {
 	require.NoError(t, r1.Close())
 	require.Equal(t, 0, rc.closed)
 
-	r2, err := src.NewReader()
+	r2, err := src.NewReadCloser()
 	require.NoError(t, err)
 	src.Seal()
 
@@ -125,27 +128,6 @@ func TestClose(t *testing.T) {
 	require.Equal(t, wantB, gotB2)
 	require.NoError(t, r1.Close())
 	require.Equal(t, 1, rc.closed)
-}
-
-func TestGenerateSample(t *testing.T) {
-	generateSampleData(t)
-}
-
-func generateSampleData(t *testing.T) {
-	f, err := os.Create(filepath.Join("testdata", "sample1.csv"))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	defer f.Close()
-
-	const line = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z\n"
-	for i := 0; i < 10000; i++ {
-		_, err = f.WriteString(strconv.Itoa(i) + "," + line)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
 }
 
 // generateSampleFile generates a temp file of sample data with the
@@ -167,10 +149,12 @@ func generateSampleFile(t *testing.T, rows int) *os.File {
 	return f
 }
 
+// readCloser is used to verify that Close was invoked the
+// expected number of times.
 type readCloser struct {
+	mu sync.Mutex
 	io.Reader
 	closed int
-	mu     sync.Mutex
 }
 
 func (rc *readCloser) Close() error {
