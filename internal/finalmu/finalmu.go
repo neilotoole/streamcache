74c36adcbec5011fceef1439da30ec8f47e77de1
neilotoole <neilotoole@apache.org>
2024-01-18 21:25:46 -0700

    Still going good

--- internal/finalmu/finalmu.go
+++ internal/finalmu/finalmu.go
@@ -15,119 +15,139 @@ var _ sync.Locker = (*Mutex)(nil)
 
 const n = 1
 
-type waiter chan struct{}
-
+// New returns a new Mutex ready for use.
 func New() *Mutex {
-	w := &Mutex{
-		reqPool: sync.Pool{New: func() any {
-			return waiter(make(chan struct{}))
-		}}}
-	return w
+	return &Mutex{waiterPool: sync.Pool{New: newWaiter}}
 }
 
-// Mutex provides a way to bound concurrent access to a resource.
-// The callers can request access with a given weight.
+// Mutex is a mutual exclusion lock whose Lock method uses a FIFO queue
+// to ensure fairness.
+//
+// A Mutex must not be copied after first use.
+//
+// The zero value for a Mutex is an unlocked mutex.
+// Mutex implements the same methodset as sync.Mutex, so it can
+// be used as a drop-in replacement.
 type Mutex struct {
-	reqPool sync.Pool
-	waiters list.List
-	cur     int64
-	mu      sync.Mutex
+	waiterPool sync.Pool
+	waiters    list.List
+	cur        int64
+	mu         sync.Mutex
 }
 
-// LockContext acquires the semaphore with a weight of n, blocking until resources
-// are available or ctx is done. On success, returns nil. On failure, returns
-// ctx.Err() and leaves the semaphore unchanged.
+// LockContext locks m.
+//
+// If the lock is already in use, the calling goroutine
+// blocks until the mutex is available or ctx is done.
+//
+// On failure, LockContext returns context.Cause(ctx.Err()) and
+// leaves the mutex unchanged.
 //
 // If ctx is already done, LockContext may still succeed without blocking.
-func (s *Mutex) LockContext(ctx context.Context) error {
-	s.mu.Lock()
-	if n-s.cur >= n && s.waiters.Len() == 0 {
-		s.cur += n
-		s.mu.Unlock()
+func (m *Mutex) LockContext(ctx context.Context) error {
+	m.mu.Lock()
+	if n-m.cur >= n && m.waiters.Len() == 0 {
+		m.cur += n
+		m.mu.Unlock()
 		return nil
 	}
 
-	w := s.reqPool.Get().(waiter)
-	elem := s.waiters.PushBack(w)
-	s.mu.Unlock()
+	if m.waiterPool.New == nil {
+		// Lazy init so that the zero value of Mutex is usable,
+		// like sync.Mutex.
+		m.waiterPool.New = newWaiter
+	}
+
+	w := m.waiterPool.Get().(waiter)
+	elem := m.waiters.PushBack(w)
+	m.mu.Unlock()
 
 	select {
 	case <-ctx.Done():
-		err := ctx.Err()
-		s.mu.Lock()
+		err := context.Cause(ctx)
+		m.mu.Lock()
 		select {
 		case <-w:
-			// Acquired the semaphore after we were canceled.  Rather than trying to
-			// fix up the queue, just pretend we didn't notice the cancelation.
+			// Acquired the lock after we were canceled.  Rather than trying to
+			// fix up the queue, just pretend we didn't notice the cancellation.
 			err = nil
-			s.reqPool.Put(w)
+			m.waiterPool.Put(w)
 		default:
-			isFront := s.waiters.Front() == elem
-			s.waiters.Remove(elem)
-			// If we're at the front and there's extra tokens left, notify other waiters.
-			if isFront && n > s.cur {
-				s.notifyWaiters()
+			isFront := m.waiters.Front() == elem
+			m.waiters.Remove(elem)
+			// If we're at the front and there's extra tokens left,
+			// notify other waiters.
+			if isFront && n > m.cur {
+				m.notifyWaiters()
 			}
 		}
-		s.mu.Unlock()
+		m.mu.Unlock()
 		return err
 
 	case <-w:
-		s.reqPool.Put(w)
+		m.waiterPool.Put(w)
 		return nil
 	}
 }
 
-func (s *Mutex) Lock() {
-	s.mu.Lock()
-	if n-s.cur >= n && s.waiters.Len() == 0 {
-		s.cur += n
-		s.mu.Unlock()
+// Lock locks m.
+//
+// If the lock is already in use, the calling goroutine
+// blocks until the mutex is available.
+func (m *Mutex) Lock() {
+	m.mu.Lock()
+	if n-m.cur >= n && m.waiters.Len() == 0 {
+		m.cur += n
+		m.mu.Unlock()
 		return
 	}
 
-	w := s.reqPool.Get().(waiter)
-	_ = s.waiters.PushBack(w)
-	s.mu.Unlock()
+	w := m.waiterPool.Get().(waiter)
+	_ = m.waiters.PushBack(w)
+	m.mu.Unlock()
 
 	<-w
-	s.reqPool.Put(w)
+	m.waiterPool.Put(w)
 	return
 }
 
-// TryLock acquires the semaphore with a weight of n without blocking.
-// On success, returns true. On failure, returns false and leaves the semaphore unchanged.
-func (s *Mutex) TryLock() bool {
-	s.mu.Lock()
-	success := n-s.cur >= n && s.waiters.Len() == 0
+// TryLock tries to lock m and reports whether it succeeded.
+func (m *Mutex) TryLock() bool {
+	m.mu.Lock()
+	success := n-m.cur >= n && m.waiters.Len() == 0
 	if success {
-		s.cur += n
+		m.cur += n
 	}
-	s.mu.Unlock()
+	m.mu.Unlock()
 	return success
 }
 
-// Unlock releases the semaphore with a weight of n.
-func (s *Mutex) Unlock() {
-	s.mu.Lock()
-	s.cur -= n
-	if s.cur < 0 {
-		s.mu.Unlock()
+// Unlock unlocks m.
+// It is a run-time error if m is not locked on entry to Unlock.
+//
+// A locked Mutex is not associated with a particular goroutine.
+// It is allowed for one goroutine to lock a Mutex and then
+// arrange for another goroutine to unlock it.
+func (m *Mutex) Unlock() {
+	m.mu.Lock()
+	m.cur -= n
+	if m.cur < 0 {
+		m.mu.Unlock()
 		panic("semaphore: released more than held")
 	}
-	s.notifyWaiters()
-	s.mu.Unlock()
+	m.notifyWaiters()
+	m.mu.Unlock()
 }
 
-func (s *Mutex) notifyWaiters() {
+func (m *Mutex) notifyWaiters() {
 	for {
-		next := s.waiters.Front()
+		next := m.waiters.Front()
 		if next == nil {
 			break // No more waiters blocked.
 		}
 
 		w := next.Value.(waiter)
-		if n-s.cur < n {
+		if n-m.cur < n {
 			// Not enough tokens for the next waiter.  We could keep going (to try to
 			// find a waiter with a smaller request), but under load that could cause
 			// starvation for large requests; instead, we leave all remaining waiters
@@ -142,8 +162,14 @@ func (s *Mutex) notifyWaiters() {
 			break
 		}
 
-		s.cur += n
-		_ = s.waiters.Remove(next)
+		m.cur += n
+		_ = m.waiters.Remove(next)
 		w <- struct{}{}
 	}
+}
+
+type waiter chan struct{}
+
+func newWaiter() any {
+	return waiter(make(chan struct{}))
 }
